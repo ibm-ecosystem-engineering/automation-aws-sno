67ba4ba350f7b3eb07175119c1652f0718f27eda
Rich Ehrhardt <51330294+rich-ehrhardt@users.noreply.github.com>
2023-02-10 11:05:33 +1100

    Initial version (#1)
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # automation-aws-snos
 Single Node OpenShift deployment on AWS
+
+# Prerequisites
+
+- The following cli tools must be installed and available in PATH
+    - aws
+    - jq
+    - terraform
+
+# Instructions
+
+1. Run `setup-workspace.sh` and follow the prompts for the required parameters. 
+
+2. Review the list of parameters and accept to setup the workspace
+
+3. Accept to continue to build when prompted
+
+# Destruction
+
+To destroy the created environment.
+
+```shell
+$ cd ./install
+$ terraform destroy
+```
+
+To clean up the workspace, run `rm -rf ./install`
+
+# Options
+
+It is possible to set the environment variables for the following prior to running `setup-workspace.sh`:
+- AWS_ACCESS_KEY_ID
+- AWS_SECRET_ACCESS_KEY
+- TF_VAR_pull_secret
---
+++ terraform/cluster.tf
@@ -0,0 +1,30 @@
+module "cluster" {
+  source = "github.com/cloud-native-toolkit/terraform-aws-ocp-sno?ref=v1.0.0"
+
+  private_subnet          = module.private_subnet.ids[0]
+  public_subnet           = module.public_subnet.ids[0]
+  vpc_cidr                = module.vpc.vpc_cidr
+
+  debug                   = false
+  install_offset          = "openshift"
+  binary_offset           = var.bin_dir
+
+  region                  = var.region
+  resource_group_name     = var.resource_group_name
+
+  access_key              = var.access_key
+  secret_key              = var.secret_key
+  base_domain_name        = var.base_domain_name
+  cluster_name            = "${var.name_prefix}-sno"
+  pull_secret             = var.pull_secret
+  public_ssh_key          = var.pub_ssh_key
+  private                 = var.private
+  use_staging_certs       = var.staging_certs
+  openshift_version       = var.openshift_version
+  node_type               = var.node_type
+  volume_type             = var.volume_type
+  volume_size             = var.volume_size
+  volume_iops             = var.volume_iops
+  update_ingress_cert     = var.update_ingress_cert
+  acme_registration_email = var.acme_email
+}
